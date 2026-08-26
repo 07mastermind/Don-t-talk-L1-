fcf9a4fdaf485cac11269569096d60191a649ad1
Md Imran <210236601+07mastermind@users.noreply.github.com>
2026-08-26 14:41:48 +0530

    Tailwind

---
+++ REACT/Day19/src/first.js
@@ -0,0 +1,133 @@
+import ReactDOM from 'react-dom/client'
+
+function App(){
+
+
+    return(
+        <div className='flex justify-center flex-wrap gap-2xl'>
+       
+        <div className='max-w-xs overflow-hidden shadow-amber-800 shadow-md m-2 p-2'>
+            <div>
+                <img className='w-full h-48 object-cover rounded-2xl ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Rt1GAMqk38XXLBezYp29U_uqiA10VzV35IChOnSyox6GPTTvcqAAzEE&s=10'></img>
+            </div>
+            <div>
+                <h1 className='font-bold text-2xl mt-2 text-gray-600'>Hello Everyone!</h1>
+            </div>
+            <div className='text-gray-500 mt-1'>
+                <p>Kya haal chaal aapke, Kaise ho aap sabloog.</p>
+            </div>
+            <div className='mt-3 flex justify-center'>
+                <button className='bg-red-500 text-amber-200 p-2 rounded-2xl hover:bg-red-600 cursor-pointer'>Click Here</button>
+            </div>
+        </div>
+
+         <div className='max-w-xs overflow-hidden shadow-amber-800 shadow-md m-2 p-2'>
+            <div>
+                <img className='w-full h-48 object-cover rounded-2xl ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Rt1GAMqk38XXLBezYp29U_uqiA10VzV35IChOnSyox6GPTTvcqAAzEE&s=10'></img>
+            </div>
+            <div>
+                <h1 className='font-bold text-2xl mt-2 text-gray-600'>Hello Everyone!</h1>
+            </div>
+            <div className='text-gray-500 mt-1'>
+                <p>Kya haal chaal aapke, Kaise ho aap sabloog.</p>
+            </div>
+            <div className='mt-3 flex justify-center'>
+                <button className='bg-red-500 text-amber-200 p-2 rounded-2xl hover:bg-red-600 cursor-pointer'>Click Here</button>
+            </div>
+        </div>
+
+        <div className='max-w-xs overflow-hidden shadow-amber-800 shadow-md m-2 p-2'>
+            <div>
+                <img className='w-full h-48 object-cover rounded-2xl ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Rt1GAMqk38XXLBezYp29U_uqiA10VzV35IChOnSyox6GPTTvcqAAzEE&s=10'></img>
+            </div>
+            <div>
+                <h1 className='font-bold text-2xl mt-2 text-gray-600'>Hello Everyone!</h1>
+            </div>
+            <div className='text-gray-500 mt-1'>
+                <p>Kya haal chaal aapke, Kaise ho aap sabloog.</p>
+            </div>
+            <div className='mt-3 flex justify-center'>
+                <button className='bg-red-500 text-amber-200 p-2 rounded-2xl hover:bg-red-600 cursor-pointer'>Click Here</button>
+            </div>
+        </div>
+
+        <div className='max-w-xs overflow-hidden shadow-amber-800 shadow-md m-2 p-2'>
+            <div>
+                <img className='w-full h-48 object-cover rounded-2xl ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Rt1GAMqk38XXLBezYp29U_uqiA10VzV35IChOnSyox6GPTTvcqAAzEE&s=10'></img>
+            </div>
+            <div>
+                <h1 className='font-bold text-2xl mt-2 text-gray-600'>Hello Everyone!</h1>
+            </div>
+            <div className='text-gray-500 mt-1'>
+                <p>Kya haal chaal aapke, Kaise ho aap sabloog.</p>
+            </div>
+            <div className='mt-3 flex justify-center'>
+                <button className='bg-red-500 text-amber-200 p-2 rounded-2xl hover:bg-red-600 cursor-pointer'>Click Here</button>
+            </div>
+        </div>
+         
+        <div className='max-w-xs overflow-hidden shadow-amber-800 shadow-md m-2 p-2'>
+            <div>
+                <img className='w-full h-48 object-cover rounded-2xl ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Rt1GAMqk38XXLBezYp29U_uqiA10VzV35IChOnSyox6GPTTvcqAAzEE&s=10'></img>
+            </div>
+            <div>
+                <h1 className='font-bold text-2xl mt-2 text-gray-600'>Hello Everyone!</h1>
+            </div>
+            <div className='text-gray-500 mt-1'>
+                <p>Kya haal chaal aapke, Kaise ho aap sabloog.</p>
+            </div>
+            <div className='mt-3 flex justify-center'>
+                <button className='bg-red-500 text-amber-200 p-2 rounded-2xl hover:bg-red-600 cursor-pointer'>Click Here</button>
+            </div>
+        </div>
+         
+        <div className='max-w-xs overflow-hidden shadow-amber-800 shadow-md m-2 p-2'>
+            <div>
+                <img className='w-full h-48 object-cover rounded-2xl ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Rt1GAMqk38XXLBezYp29U_uqiA10VzV35IChOnSyox6GPTTvcqAAzEE&s=10'></img>
+            </div>
+            <div>
+                <h1 className='font-bold text-2xl mt-2 text-gray-600'>Hello Everyone!</h1>
+            </div>
+            <div className='text-gray-500 mt-1'>
+                <p>Kya haal chaal aapke, Kaise ho aap sabloog.</p>
+            </div>
+            <div className='mt-3 flex justify-center'>
+                <button className='bg-red-500 text-amber-200 p-2 rounded-2xl hover:bg-red-600 cursor-pointer'>Click Here</button>
+            </div>
+        </div>
+
+        <div className='max-w-xs overflow-hidden shadow-amber-800 shadow-md m-2 p-2'>
+            <div>
+                <img className='w-full h-48 object-cover rounded-2xl ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Rt1GAMqk38XXLBezYp29U_uqiA10VzV35IChOnSyox6GPTTvcqAAzEE&s=10'></img>
+            </div>
+            <div>
+                <h1 className='font-bold text-2xl mt-2 text-gray-600'>Hello Everyone!</h1>
+            </div>
+            <div className='text-gray-500 mt-1'>
+                <p>Kya haal chaal aapke, Kaise ho aap sabloog.</p>
+            </div>
+            <div className='mt-3 flex justify-center'>
+                <button className='bg-red-500 text-amber-200 p-2 rounded-2xl hover:bg-red-600 cursor-pointer'>Click Here</button>
+            </div>
+        </div>
+
+        <div className='max-w-xs overflow-hidden shadow-amber-800 shadow-md m-2 p-2'>
+            <div>
+                <img className='w-full h-48 object-cover rounded-2xl ' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Rt1GAMqk38XXLBezYp29U_uqiA10VzV35IChOnSyox6GPTTvcqAAzEE&s=10'></img>
+            </div>
+            <div>
+                <h1 className='font-bold text-2xl mt-2 text-gray-600'>Hello Everyone!</h1>
+            </div>
+            <div className='text-gray-500 mt-1'>
+                <p>Kya haal chaal aapke, Kaise ho aap sabloog.</p>
+            </div>
+            <div className='mt-3 flex justify-center'>
+                <button className='bg-red-500 text-amber-200 p-2 rounded-2xl hover:bg-red-600 cursor-pointer'>Click Here</button>
+            </div>
+        </div>
+
+        </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
